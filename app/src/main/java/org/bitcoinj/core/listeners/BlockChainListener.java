dcc91c389158c95d3bcde84a8932b88cee3df073
Antonio Ferreira(Genesis) <fulltimegeek@protonmail.com>
2016-01-21 13:20:03 -0500

    separated dashj package

--- app/src/main/java/org/bitcoinj/core/listeners/BlockChainListener.java
+++
@@ -1,8 +0,0 @@
-package org.bitcoinj.core.listeners;
-
-/**
- * Old interface for backwards compatibility. Implement the more specific interfaces instead.
- */
-@Deprecated
-public interface BlockChainListener extends NewBestBlockListener, TransactionReceivedInBlockListener, ReorganizeListener {
-}
